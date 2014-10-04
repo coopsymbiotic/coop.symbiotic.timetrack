ec0c0c875d5532c59ae19962543931b97bd43cdd
Mathieu Lutfy <mathieu@bidon.ca>
2014-10-04 12:17:10 -0400

    CRM/Timetrack/Case/Page/CaseView.php: add list of invoices.

--- CRM/Timetrack/Case/Page/CaseView.php
+++ CRM/Timetrack/Case/Page/CaseView.php
@@ -23,7 +23,7 @@ class CRM_Timetrack_Case_Page_CaseView {
         $actions = array(
           CRM_Utils_System::href(ts('Add punch'), 'civicrm/timetrack/punch/', array('reset' => 1, 'cid' => $case_id, 'action' => 'create')),
           CRM_Utils_System::href(ts('View all punches'), 'node/' . $kcontract_nid . '/punches'),
-          CRM_Utils_System::href(ts('View billing'), 'node/' . $kcontract_nid . '/kbpill'),
+          // CRM_Utils_System::href(ts('View billing'), 'node/' . $kcontract_nid . '/kbpill'),
         );
 
         $summary['kproject'] = array(
@@ -31,9 +31,19 @@ class CRM_Timetrack_Case_Page_CaseView {
           'value' => implode(', ', $actions),
         );
 
+        $summary['billing_status'] = array(
+          'label' => ts('Billing status'),
+          'value' => ts('%1 unbilled hour(s)', array(1 => CRM_Timetrack_Utils::roundUpSeconds($this->getUnbilledHours($case_id)))),
+        );
+
         $summary['ktasks'] = array(
-          'label' => ts('Tasks:'),
-          'value' => kproject_tasks_list($node),
+          'label' => '',
+          'value' => '<div id="crm-timetrack-caseview-tasks" class="crm-accordion-wrapper"><div class="crm-accordion-header">Tasks</div><div class="crm-accordion-body">' . kproject_tasks_list($node) . '</div></div>',
+        );
+
+        $summary['kinvoices'] = array(
+          'label' => '',
+          'value' => $this->getListOfInvoice($case_id),
         );
       }
     }
@@ -45,5 +55,63 @@ class CRM_Timetrack_Case_Page_CaseView {
     }
 
     return $summary;
+  }
+
+  function getListOfInvoice($case_id) {
+    $smarty = CRM_Core_Smarty::singleton();
+
+    $smarty->assign('timetrack_header_idcss', 'caseview-invoices');
+    $smarty->assign('timetrack_header_title', ts('Invoices', array('domain' => 'ca.bidon.timetrack')));
+
+    // FIXME ts() domain.
+    $headers = array(
+      'subject' => ts('Title'),
+      'total' => ts('Total punches'),
+      'invoiced' => ts('Invoiced'),
+      'invoiced_pct' => ts('% invoiced'),
+      'ledger_id' => ts('Ledger ID'),
+      'state' => ts('Status'),
+    );
+
+    $smarty->assign('timetrack_headers', $headers);
+
+    $rows = array();
+
+    $result = civicrm_api3('Timetrackinvoice', 'get', array(
+      'case_id' => $case_id,
+      'option.limit' => 1000,
+    ));
+
+    foreach ($result['values'] as $invoice) {
+      $included_hours = CRM_Timetrack_Utils::roundUpSeconds($invoice['total_included'], 1);
+
+      $rows[] = array(
+        'subject' => CRM_Utils_System::href($invoice['subject'], 'node/' . $invoice['invoice_id'] . '/edit'),
+        'total' => $included_hours,
+        'invoiced' => $invoice['hours_billed'], // already in hours
+        'invoiced_pct' => ($included_hours > 0 ? round($invoice['hours_billed'] / $included_hours * 100, 2) : 0) . '%',
+        'state' => $invoice['state'],
+        'ledger_id' => $invoice['ledger_bill_id'],
+      );
+    }
+
+    $smarty->assign('timetrack_rows', $rows);
+
+    return $smarty->fetch('CRM/Timetrack/Page/Snippets/AccordionTable.tpl');
+  }
+
+  function getUnbilledHours($case_id) {
+    // TODO: move to API ?
+    $dao = CRM_Core_DAO::executeQuery('SELECT sum(duration) as total
+      FROM kpunch
+      LEFT JOIN ktask on (ktask.nid = kpunch.nid)
+      LEFT JOIN civicrm_value_infos_base_contrats_1 as bc on (bc.kproject_node_2 = ktask.parent)
+      WHERE bc.entity_id = %1
+        AND order_reference is NULL', array(
+      1 => array($case_id, 'Positive'),
+    ));
+
+    $dao->fetch();
+    return $dao->total;
   }
 }
