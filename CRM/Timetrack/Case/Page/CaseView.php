56e69dc1a2820f7f3e5b16ad47403d8b637ad2e6
Mathieu Lutfy <mathieu@symbiotic.coop>
2017-09-02 18:56:37 -0400

    CaseView: list of invoices: link the invoide ID, not the date.

--- CRM/Timetrack/Case/Page/CaseView.php
+++ CRM/Timetrack/Case/Page/CaseView.php
@@ -204,7 +204,8 @@ class CRM_Timetrack_Case_Page_CaseView {
       $included_hours = CRM_Timetrack_Utils::roundUpSeconds($invoice['total_included'], 1);
 
       $rows[] = array(
-        'created_date' => CRM_Utils_System::href(substr($invoice['created_date'], 0, 10), 'civicrm/timetrack/invoice', array('invoice_id' => $invoice['invoice_id'])),
+        'ledger_id' => CRM_Utils_System::href($invoice['ledger_bill_id'], 'civicrm/timetrack/invoice', array('invoice_id' => $invoice['invoice_id'])),
+        'created_date' => substr($invoice['created_date'], 0, 10),
         'total' => $included_hours,
         'invoiced' => $invoice['hours_billed'], // already in hours
         'invoiced_pct' => ($included_hours > 0 ? round($invoice['hours_billed'] / $included_hours * 100, 2) : 0) . '%',
@@ -217,7 +218,6 @@ class CRM_Timetrack_Case_Page_CaseView {
         'deposit_reference' => "<div class='crm-entity' data-entity='Timetrackinvoice' data-id='{$invoice['id']}'>"
           . "<div class='crm-editable' data-type='text' data-field='deposit_reference'>" . $invoice['deposit_reference'] . '</div>'
           . '</div>',
-        'ledger_id' => $invoice['ledger_bill_id'],
         'generate' => CRM_Utils_System::href(ts('View'), 'civicrm/timetrack/invoice', array('invoice_id' => $invoice['invoice_id']))
           . ', ' . CRM_Utils_System::href(ts('Generate'), 'civicrm/timetrack/invoice/generate', array('invoice_id' => $invoice['invoice_id']))
           . ', ' . CRM_Utils_System::href(ts('Copy'), 'civicrm/timetrack/invoice', array('invoice_id' => $invoice['invoice_id'], 'action' => 'clone'))
