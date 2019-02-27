d5dd37ab24472bd012141a7fe4c2752058a648a0
Tim Otten <totten@civicrm.org>
2019-02-27 00:09:26 -0800

    Update civix boilerplate

--- timetrack.php
+++ timetrack.php
@@ -100,6 +100,20 @@ function timetrack_civicrm_caseTypes(&$caseTypes) {
 }
 
 /**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function timetrack_civicrm_angularModules(&$angularModules) {
+  _timetrack_civix_civicrm_angularModules($angularModules);
+}
+
+/**
  * Implementation of hook_civicrm_alterSettingsFolders
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
