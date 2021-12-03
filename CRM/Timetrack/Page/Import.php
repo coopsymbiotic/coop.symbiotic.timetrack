bf12dec61a184e6660a0e0878999ee1f8b8252fa
Mathieu Lutfy <mathieu@symbiotic.coop>
2021-12-03 16:16:18 +0000

    Timetrack Import: CiviCRM 5.37+ compatibility

--- CRM/Timetrack/Page/Import.php
+++ CRM/Timetrack/Page/Import.php
@@ -7,14 +7,13 @@ class CRM_Timetrack_Page_Import extends CRM_Core_Page {
    * Run page
    */
   public function run() {
-    $loader = new \Civi\Angular\AngularLoader();
-    $loader->setModules(['timetrackImport']);
+    $loader = Civi::service('angularjs.loader');
     $loader->setPageName('civicrm/timetrack/import');
+    $loader->addModules(['crmApp', 'timetrackImport']);
     $loader->useApp([
       'defaultRoute' => '/import',
     ]);
     $loader->load();
-    CRM_Utils_System::setTitle(E::ts('Timetrack: Import time-punches'));
     parent::run();
   }
 
