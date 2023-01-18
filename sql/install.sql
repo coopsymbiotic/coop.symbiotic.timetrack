0607827d31b58cbc537558c646637ee0e1b758a8
Mathieu Lutfy <mathieu@symbiotic.coop>
2023-01-18 11:40:07 -0500

    sql/install: fix invalid FK, fatal on installation

--- sql/install.sql
+++ sql/install.sql
@@ -93,6 +93,6 @@ CREATE TABLE `kpunch` (
   CONSTRAINT `FK_kpunch_contact_id` FOREIGN KEY (`contact_id`) REFERENCES `civicrm_contact` (`id`) ON DELETE SET NULL,
   CONSTRAINT `FK_kpunch_korder_id` FOREIGN KEY (`korder_id`) REFERENCES `korder` (`id`) ON DELETE SET NULL,
   CONSTRAINT `FK_kpunch_korder_line_id` FOREIGN KEY (`korder_line_id`) REFERENCES `korder_line` (`id`) ON DELETE SET NULL,
-  CONSTRAINT `FK_kpunch_line_item_id` FOREIGN KEY (`korder_line_item_id`) REFERENCES `civicrm_line_item` (`id`) ON DELETE SET NULL,
+  CONSTRAINT `FK_kpunch_line_id` FOREIGN KEY (`korder_line_id`) REFERENCES `civicrm_line_item` (`id`) ON DELETE SET NULL,
   CONSTRAINT `FK_ktask_id` FOREIGN KEY (`ktask_id`) REFERENCES `civicrm_timetracktask` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
